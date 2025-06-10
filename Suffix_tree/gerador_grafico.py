55efdb31e6bf7d0dec1f4586310e634b5b8d987c
Giani Augusto Braga <giani.braga80@gmail.com>
2025-06-10 11:28:22 -0300

    update gráficos
    Ajustado os gráficos,  para conter o desvio padrão.

--- Suffix_tree/gerador_grafico.py
+++ Suffix_tree/gerador_grafico.py
@@ -1,23 +1,30 @@
 import matplotlib.pyplot as plt
 
-# Dados de exemplo (você pode substituir pelos seus valores reais)
-# Tamanhos das entradas (valores de TAMANHO_STRING usados no C)
-tamanhos = [100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000] 
+# Tamanhos das entradas
+tamanhos = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000] 
 
-# Tempos de execução correspondentes (em segundos)
-tempos = [0.001200, 0.001200, 0.001600, 0.003400, 0.005600, 0.001800, 0.003600, 0.003000, 0.005400, 0.007600, 0.003800, 0.012200, 0.022200, 0.011000, 0.014200 , 0.005000 ,0.013400 , 0.020800 , 0.018400 , 0.027200 ,0.022000 ,0.008000 , 0.017600 , 0.019800 , 0.016400 , 0.022600 , 0.019000  ,0.032800  , 0.020200 , 0.021600 , 0.025600 , 0.020800 , 0.030400 , 0.040400 , 0.035800 , 0.028000 , 0.017600 , 0.031800 , 0.021200]  # substitua pelos seus
+# Tempos de execução
+tempos = [0.000322, 0.000491, 0.000812, 0.000924, 0.001251, 0.001386, 0.001523, 0.001624, 0.001843, 0.001960, 0.002078, 0.002110, 0.002191, 0.002224, 0.002352, 0.002373, 0.002401, 0.002742, 0.002783 , 0.002823 , 0.003010, 0.003160, 0.003335, 0.003543, 0.003613 , 0.003879, 0.003966, 0.004167, 0.004200, 0.004260]
 
-# Criando o gráfico de linha
+# Desvios padrão correspondentes
+desvios = [0.000097, 0.000212, 0.000428, 0.000360, 0.000955, 0.000959, 0.001259, 0.001649, 0.001431, 0.001730, 0.001589, 0.001678, 0.001640, 0.001949, 0.002003, 0.001956, 0.001901, 0.002543, 0.002432, 0.002997, 00.002643, 0.002821, 0.003168, 0.002943, 0.003425, 0.002919, 0.004007, 0.004284, 0.003121, 0.003536]
+
+# Criando o gráfico
 plt.figure(figsize=(30, 6))
-plt.plot(tamanhos, tempos, marker='o', linestyle='-', color='blue', linewidth=2)
+
+# Linha do tempo
+plt.plot(tamanhos, tempos, marker='o', linestyle='-', color='blue', linewidth=2, label='Tempo de Execução')
+
+# Linha do desvio padrão
+plt.plot(tamanhos, desvios, marker='x', linestyle='--', color='red', linewidth=2, label='Desvio Padrão')
 
 # Títulos e rótulos
-plt.title('Crescimento do Tempo de Execução x Tamanho da Entrada (LRS Suffix tree)', fontsize=14)
+plt.title('Tempo de Execução vs Tamanho da Entrada (com Desvio Padrão)', fontsize=14)
 plt.xlabel('Tamanho da String (n)', fontsize=12)
-plt.ylabel('Tempo de Execução (segundos)', fontsize=12)
-# plt.ylim(0, 1) 
+plt.ylabel('Tempo (segundos)', fontsize=12)
 plt.grid(True)
 plt.xticks(tamanhos)
+plt.legend()
 plt.tight_layout()
 
 # Exibe o gráfico
--- brute_force/grafico.py
+++ brute_force/grafico.py
@@ -5,19 +5,29 @@ import matplotlib.pyplot as plt
 tamanhos = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000]
 
 # Tempos de execução correspondentes (em segundos)
-tempos = [0.0040, 0.0128, 0.0380, 0.0728, 0.1488, 0.2366, 0.3484, 0.4874, 0.6852, 1.0792, 1.2492, 1.6146, 2.0414, 2.7604, 3.3856, 3.8530, 4.4776, 5.2648 , 11.6386 , 12.5002, 8.3808, 9.9332, 11.0734, 12.7002, 14.3946, 16.1742, 23.8126, 21.6882, 22.8996, 25.6902]  # substitua pelos seus
+tempos = [0.0040, 0.0128, 0.0380, 0.0728, 0.1488, 0.2366, 0.3484, 0.4874, 0.6852, 1.0792, 1.2492, 1.6146, 2.0414, 2.7604, 3.3856, 3.8530, 4.4776, 5.2648 , 6.6386  , 7.5002, 8.3808, 9.9332, 11.0734, 12.7002, 14.3946, 16.1742, 19.8126, 21.6882, 22.8996, 25.6902]  # substitua pelos seus
 
-# Criando o gráfico de linha
-plt.figure(figsize=(15, 6))
-plt.plot(tamanhos, tempos, marker='o', linestyle='-', color='blue', linewidth=2)
+# Desvios padrão correspondentes
+desvios = [0.0007, 0.0040, 0.0130, 0.0198, 0.0372, 0.0402,  0.0415, 0.0333, 0.0467, 0.0426, 0.0608, 0.0311, 0.0546, 0.1385, 0.1315, 0.0860, 0.0999, 0.1723, 0.1469, 0.1488, 0.1567, 0.0508, 0.1881, 0.1582, 0.4935, 0.4347, 1.0975, 1.1245, 0.8569, 1.3152]
+
+# Criando o gráfico
+plt.figure(figsize=(30, 6))
+
+# Linha do tempo
+plt.plot(tamanhos, tempos, marker='o', linestyle='-', color='blue', linewidth=2, label='Tempo de Execução')
+
+# Linha do desvio padrão
+plt.plot(tamanhos, desvios, marker='x', linestyle='--', color='red', linewidth=2, label='Desvio Padrão')
 
 # Títulos e rótulos
-plt.title('Crescimento do Tempo de Execução x Tamanho da Entrada (LRS Força Bruta)', fontsize=14)
+plt.title('Tempo de Execução vs Tamanho da Entrada (com Desvio Padrão)', fontsize=14)
 plt.xlabel('Tamanho da String (n)', fontsize=12)
-plt.ylabel('Tempo de Execução (segundos)', fontsize=12)
+plt.ylabel('Tempo (segundos)', fontsize=12)
 plt.grid(True)
 plt.xticks(tamanhos)
+plt.legend()
 plt.tight_layout()
+
 
 # Exibe o gráfico
 plt.show()
